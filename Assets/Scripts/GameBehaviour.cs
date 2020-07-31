52e0af8c1088ae068936400a7da2ab9de4bb74bd
Ege Sucu <ege@softhion.com>
2020-08-01 01:02:28 +0300

    Code Formatting

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -6,23 +6,23 @@ public class GameBehaviour : MonoBehaviour
     private AudioSource _audioSource;
     [SerializeField] private float rcsThrust = 80f;
     [SerializeField] private float mainThrust = 10f;
-
-
+    
     private void Start()
     {
+        // Getting Components from the Unity Editor
         _audioSource = GetComponent<AudioSource>();
         _rigidbody = GetComponent<Rigidbody>();
     }
-
-
+    
     private void Update()
     {
+        // Manage Rocket Movement
         ProcessInput();
     }
-
-
+    
     private void OnCollisionEnter(Collision other)
     {
+        // Collision Checker when the object hit the "Dead-Zones" for Game Lost and "Win-Zones" for Game Win.
         switch (other.gameObject.tag)
         {
             case "Dead-Zone":
@@ -37,26 +37,27 @@ public class GameBehaviour : MonoBehaviour
         }
     }
 
-
     private void ProcessInput()
     {
+        // Add the Movement support
         Thrust();
+        // Add the Left-Right Movement supports
         Rotate();
     }
 
     private void Rotate()
     {
         var frameRotationSpeed = rcsThrust * Time.deltaTime;
+        
         _rigidbody.freezeRotation = true;
-
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward * frameRotationSpeed);
+            RotateLeft(frameRotationSpeed);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(Vector3.back * frameRotationSpeed);
+           RotateRight(frameRotationSpeed);
         }
 
         _rigidbody.freezeRotation = false;
@@ -66,16 +67,40 @@ public class GameBehaviour : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            if (!_audioSource.isPlaying)
-            {
-                _audioSource.Play();
-            }
-
-            _rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+            StartAudio();
+            AddForce();
         }
         else
         {
-            _audioSource.Stop();
+            StopAudio();
         }
+    }
+
+    private void StartAudio()
+    {
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.Play();
+        }
+    }
+
+    private void AddForce()
+    {
+        _rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+    }
+
+    private void StopAudio()
+    {
+        _audioSource.Stop();
+    }
+
+    private void RotateLeft(float frameRotationSpeed)
+    {
+        transform.Rotate(Vector3.forward * frameRotationSpeed);
+    }
+
+    private void RotateRight(float frameRotationSpeed)
+    {
+        transform.Rotate(Vector3.back * frameRotationSpeed);
     }
 }
